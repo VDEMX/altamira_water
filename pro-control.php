e3f5d38384ec47cc5f0f5810137341d4d4da53a3
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-09-30 16:35:36 -0500

    Borrar archivos obsoletos

--- pro-control.php
+++ pro-control.php
@@ -1,22 +1,9 @@
 <?php $active_page="productos"; ?>
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if IE 9]>         <html class="no-js ie9"> <![endif]-->
-<!--[if gt IE 9]><!--> <html class="no-js" lang="es"> <!--<![endif]-->
- 	<head>  
- 		<meta charset="utf-8">
- 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
- 		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0; target-densitydpi=160dpi;">
- 		<title>Altamira Water Ltda. | SERIE PROCONTROL</title>
+<?php include("head.php"); ?>
+ 		<title><?php echo "$nombre"?> | SERIE PROCONTROL</title>
  		<meta name="description" content="PROCONTROL es un dispositivo electrónico especialmente diseñado para la Protección y Control de motores monofásicos y trifásicos.">
  		<meta name="keywords" content="PROCONTROL, dispositivo, electrónico, Protección, Control, motores, monofásicos, trifásicos">
- 		<meta name="robots" content="Index, follow" />
- 		
- 		<!-- Estilos -->
- 		<link rel="stylesheet" href="http://necolas.github.io/normalize.css/2.1.1/normalize.css" media="all">
- 		<link rel="stylesheet" href="css/styles.css" media="all">
-		
- 		<!-- Modernizr -->
- 		<script src="http://modernizr.com/downloads/modernizr.js"></script>
+ 		<meta name="robots" content="Index, follow">
  	</head>
  	
  <body>
@@ -68,7 +55,8 @@ servicio.
  		</section>
  		<section class="doscol">
 	 		<img src="images/procontrol-vector.gif" alt="Vector">
-	 		<a href="http://vde.com.mx/fichas-tecnicas/download.php?file=08_FTHC-PROCONTROL-1-130704.pdf"><img src="images/btn-descargar.png" alt="btn-descargar"></a>
+	 		<a href="http://vde.com.mx/fichas-tecnicas/download.php?file=08_FTHC-PROCONTROL-1-130704.pdf"><button type="button" class="button"> <div class="icon icon-descargar"></div> Descargar ficha técnica</button></a>
+	 		<a href="http://vde.com.mx/fichas-tecnicas/08_FTHC-PROCONTROL-1-130704.pdf" target="_blank"><button type="button" class="button"> <div class="icon icon-ojo"></div> Visualizar ficha técnica</button></a>
 	 		
 		 	<h2>Código del equipo</h4>
 		 	<img src="images/procontrol-codigo.gif" alt="Código del equipo">
@@ -85,7 +73,5 @@ servicio.
 		<div class="clear"></div>
 	</section>
  	
- 	<!-- !Iframe -->
- 	<iframe src="machote.html" width="800" height="450" name="contenido" frameborder="0" valign="top" bgcolor="#6796C2" class="inactive"></iframe>
- 	
+ 	<?php include("iframe.php"); ?>
  	<?php include("footer.php"); ?>
