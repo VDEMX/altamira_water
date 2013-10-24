0a14ca9ccb870926b6800c10e39b1a368ae20e47
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-10-24 10:53:02 -0500

    Actualizar la información de la familia Kor

--- menu.php
+++ menu.php
@@ -6,11 +6,9 @@
       <ul>
          <li class="has-sub"><a href="#"><span>Bombas Sumergibles para Pozo</span></a>
             <ul>
-               <li class="boton"><a href="vde/bkor.html" target="contenido"><span>Bombas Altamira Serie KOR (Acero Inoxidable)</span></a></li>
-               <li class="boton"><a href="vde/mba.html" target="contenido"><span>Bombas Altamira Serie BS (Hierro) y Serie T</span></a></li>
-               <li class="boton"><a href="vde/bkorac.htm" target="contenido"><span>Bombas Altamira Serie KOR Altas Cargas</span></a></li>
-               <li class="boton"><a href="vde/bom_sum_aqua.html" target="contenido"><span>Bombas Aqua Pak (Serie Max y Task)</span></a></li>
-               <li class="last boton"><a href="vde/mbe.html" target="contenido"><span>Bombas Espa (Acuaria)</span></a></li>
+               <li><a href="/kor"><span>Altamira Serie KOR (Acero Inoxidable)</span></a></li>
+               <li class="boton"><a href="vde/mba.html" target="contenido"><span>Altamira Serie BS (Hierro) y Serie T</span></a></li>
+               <li class="boton"><a href="vde/bom_sum_aqua.html" target="contenido"><span>Aqua Pak (Serie Max y Task)</span></a></li>
             </ul>
          </li>
          <li class="has-sub"><a href="#"><span>Motores Sumergibles</span></a>
