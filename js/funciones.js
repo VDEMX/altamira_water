73a03f81250f66ae9ebf80f2077c357a6dff1af4
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-11-29 10:38:18 -0600

    iframe con alto automático

--- js/funciones.js
+++ js/funciones.js
@@ -24,12 +24,30 @@ function inicio ()
         responseDiv : '#respuesta',
         allBrowsers: true,  
     });
+     //Simular el evento hover al hacer click en pantallas táctiles
+    var myLinks = document.getElementsByTagName('a');
+	for(var i = 0; i < myLinks.length; i++){
+	   myLinks[i].addEventListener('touchstart', function(){this.className = "hover";}, false);
+	   myLinks[i].addEventListener('touchend', function(){this.className = "";}, false);
+	}
      $('.vbox-close').click(
       function ocultar()
       {
            $(".vbox-overlay").toggle();
       }
    );
+}
+
+function calcHeight()
+{
+	 //Cojo la altura en nuestra página
+	 var the_height=
+	 document.getElementById
+	('iframe').contentWindow.
+	 document.body.scrollHeight;
+	//Cambio la altura del iframe
+	 document.getElementById('iframe')
+	.height= the_height;
 }
 
 // Scripts para iOS
@@ -46,12 +64,30 @@ window.addEventListener("load",function cargar() {
   
 });
 
+// Mobile Safari in standalone mode
+if(("standalone" in window.navigator) && window.navigator.standalone){
+ 
+	// If you want to prevent remote links in standalone web apps opening Mobile Safari, change 'remotes' to true
+	var noddy, remotes = false;
+	
+	document.addEventListener('click', function(event) {
+		
+		noddy = event.target;
+		
+		// Bubble up until we hit link or top HTML element. Warning: BODY element is not compulsory so better to stop on HTML
+		while(noddy.nodeName !== "A" && noddy.nodeName !== "HTML") {
+	        noddy = noddy.parentNode;
+	    }
+		
+		if('href' in noddy && noddy.href.indexOf('http') !== -1 && (noddy.href.indexOf(document.location.host) !== -1 || remotes))
+		{
+			event.preventDefault();
+			document.location.href = noddy.href;
+		}
+	
+	},false);
+}
+
 // bloquear el zoom en iOS
 MBP.scaleFix();
 }
-
-// Respond.js
-yepnope({
-	test : Modernizr.mq('(only all)'),
-	nope : ['respond.min.js']
-});
